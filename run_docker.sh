b9b6ca6450271c6245b181c36c29a7ca0c76e90c
Ubuntu <ubuntu@ip-172-31-7-40.us-west-2.compute.internal>
2020-07-18 23:06:28 +0000

    third

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,11 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=app-4 .
 
 # Step 2: 
 # List docker images
-
+docker images 
 # Step 3: 
 # Run flask app
+docker run -p 8888:80 app 
