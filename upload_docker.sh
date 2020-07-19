acfa4def2d40e3d0ba294a85d1a55767acc89aa4
Ubuntu <ubuntu@ip-172-31-7-40.us-west-2.compute.internal>
2020-07-19 10:19:29 +0000

    tenth commit

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ahmedbendo/app-4
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker login --username=ahmedbendo
+docker tag app-4 ahmedbendo/udacity-app-4
+docker push ahmedbendo/udacity-app-4
+
